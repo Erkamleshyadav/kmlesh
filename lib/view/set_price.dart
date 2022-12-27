5f74fa983b7ebf90918fd11835f5ff81517de616
kamlesh <115397147+softdevkam@users.noreply.github.com>
2022-12-27 13:14:56 +0530

    Update set_price.dart

--- lib/view/set_price.dart
+++ lib/view/set_price.dart
@@ -15,10 +15,72 @@ class CreatingBill extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              TextFormField(),
-              TextFormField(),
-              TextFormField(),
+              Center(child: Text('Create Bill for customer')),
+              SizedBox(height: 8),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                cursorColor: Colors.black,
+                cursorHeight: 30,
+                cursorRadius: Radius.circular(10),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: 'Day Meter Reading',
+                  filled: true,
+                  fillColor: Colors.green.shade50,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.purpleAccent.shade100,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                cursorColor: Colors.black,
+                cursorHeight: 30,
+                cursorRadius: Radius.circular(10),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: 'Night Meter Reading',
+                  filled: true,
+                  fillColor: Colors.green.shade50,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.purpleAccent.shade100,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                cursorColor: Colors.black,
+                cursorHeight: 30,
+                cursorRadius: Radius.circular(10),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  hintText: 'Gas Meter Reading',
+                  filled: true,
+                  fillColor: Colors.green.shade50,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.purpleAccent.shade100,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
+              ElevatedButton(onPressed: (() {}), child: Text('Update'))
             ],
           ),
         ),
